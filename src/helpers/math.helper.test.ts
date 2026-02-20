50b7d08d168f388363adaa2afd4b34bfe5a86aa1
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-02-19 19:46:17 -0500

    feat: test function substract & multiply

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,8 +1,40 @@
-import { expect, test } from "vitest";
-import { add } from "./math.helper";
+import { expect, test, describe } from "vitest";
+import { add, multiply, substract } from "./math.helper";
+//* Testing automatico
+describe("add", () => {
+  test("should add two positive numbers", () => {
+    // ! arrange
+    const result = add(1, 2);
+    // ! Act
+    console.log(result);
+    //! Assert
+    expect(result).toBe(3);
+  });
+});
 
-test("add function from cart", () => {
-  const result = add(1, 2);
-  console.log(result);
-  expect(result).toBe(4);
+describe("subscribe", () => {
+  test("should rest two numbers", () => {
+    // ! arrange
+    const result = substract(1, 2);
+    // ! Act
+    console.log(result);
+    //! Assert
+    expect(result).toBe(-1);
+  });
+
+  test("debe devolver la resta de 4-4", () => {
+    const result = substract(4, 4);
+    expect(result).toBe(0);
+  });
+});
+
+describe("multiplicación", () => {
+  test("debe devolver 4*5", () => {
+    const result = multiply(4, 5);
+    expect(result).toBe(20);
+  });
+  test("debe devolver 7*5", () => {
+    const result = multiply(7, 5);
+    expect(result).toBe(35);
+  });
 });
